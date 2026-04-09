8b7e0ada78ed5672855df321b63df3039109f099
Tuhina-Paria <pariatuhina04@gmail.com>
2026-04-09 08:17:28 +0530

    fix tailwind error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
-exports = {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'popx-purple': '#6C25FF',
+        'popx-light-purple': '#CBC3E3',
+        'popx-gray': '#ECECEC',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
